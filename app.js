ab0d5c66c108be2b10727c90f4a9078aa4a192be
Axlejandro <aalejandro.armesto@gmail.com>
2022-07-13 18:10:59 -0300

    boton de registo

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.use(express.static(pathPublic));
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
-
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
 app.listen(5000, ()=>{
     console.log('Servidor funcionando en: http://localhost:5000');
 });
