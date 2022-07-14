9a4ef5a3f981f24e312ef3e9dd88b451c9e01ae2
EliVelezA <eliza_velez@hotmail.com>
2022-07-14 00:29:31 -0500

    se agrega pagina home

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 const pathPublic = path.resolve(__dirname, './public');
 app.use(express.static(pathPublic));
 
 app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+}); 
+
+app.post('/', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
 app.get('/register', (req,res)=>{
